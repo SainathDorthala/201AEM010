847c5acce1b5ac3a1121553e22894499457fa0d6
SainathDorthala <63204757+SainathDorthala@users.noreply.github.com>
2020-05-18 23:25:41 +0300

    Add files via upload

--- test.py
+++ test.py
@@ -5,25 +5,14 @@ connection = sqlite3.connect('data.db')
 cursor = connection.cursor()
 
 create_table = "CREATE TABLE users (id int, username text, password text)"
-cursor.execute(create_table)
+cursor.execute( create_table )
 
-user = (1, 'sravani', '12345')
+user = (1, 'sainathdorthala', '12345')
 insert_query = "INSERT INTO users VALUES (?, ?, ?)"
 cursor.execute(insert_query, user)
-print (user)
-user = (2, 'eve', '56789')
+user = (2, 'sainathdorthala', '56789')
 insert_query = "INSERT INTO users VALUES (?, ?, ?)"
 cursor.execute(insert_query, user)
-print (user)
-user = (3, 'bob', '13579')
+user = (3, 'sainathdorthala', '67891')
 insert_query = "INSERT INTO users VALUES (?, ?, ?)"
 cursor.execute(insert_query, user)
-print (user)
-user = (4, 'elena', '24680')
-insert_query = "INSERT INTO users VALUES (?, ?, ?)"
-cursor.execute(insert_query, user)
-print (user)
-user = (5, 'stefan', '12569')
-insert_query = "INSERT INTO users VALUES (?, ?, ?)"
-cursor.execute(insert_query, user)
-print (user)
